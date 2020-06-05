8f523062f9fcbe1f2c28de01069467fabba17dac
rckstrjanu <rckstrjanu@gmail.com>
2020-06-05 20:49:22 +0000

    IP Permissions is created

--- executeexasolstatements.py
+++ executeexasolstatements.py
@@ -3,14 +3,21 @@
 import pyexasol
 import sys
 
-# GET DB CONNECTION
-Con = pyexasol.connect(dsn="localhost:8888", user="sys", password ="exasol", schema ="TEST", compression=True);
-
-user=str(sys.argv[1]);
-password=str(sys.argv[2]);
-role=str(sys.argv[3])
+connectedip=str(sys.argv[1]);
+user=str(sys.argv[2]);
+password=str(sys.argv[3]);
+role=str(sys.argv[4])
 print("user: "+user);
 print("password: "+password);
+
+# GET DB CONNECTION
+Con = pyexasol.connect(dsn= connectedip+":8888", user="sys", password ="exasol", schema ="TEST", compression=True);
+
+#user=str(sys.argv[2]);
+#password=str(sys.argv[3]);
+#role=str(sys.argv[4])
+#print("user: "+user);
+#print("password: "+password);
 
 # CREATE A USER IN DATABASE
 Con.execute("CREATE USER "+user+" IDENTIFIED BY "+password);
